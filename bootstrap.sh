509bc8b0084e2a00406e0ffbe67369a29b723bd7
Ivan Hamilton <ivan@chimerical.com.au>
2015-11-03 16:56:42 +1100

    Latest n98-magerun is at magerun.net and not github.com chmod after move (vboxfs won't preserve flags on Windows host)

--- bootstrap.sh
+++ bootstrap.sh
@@ -119,6 +119,6 @@ fi
 # Install n98-magerun
 # --------------------
 cd /vagrant/httpdocs
-wget https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar
-chmod +x ./n98-magerun.phar
+wget http://files.magerun.net/n98-magerun-latest.phar -O n98-magerun.phar --progress dot:mega
 sudo mv ./n98-magerun.phar /usr/local/bin/
+chmod +x /usr/local/bin/n98-magerun.phar
